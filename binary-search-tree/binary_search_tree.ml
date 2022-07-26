b0159616587fe0f256e7eb84d2bc814d191cb32e
2Paradise <robinnw@naver.com>
2022-07-26 13:58:45 +0900

    fix: solution binary search tree

--- binary-search-tree/binary_search_tree.ml
+++ binary-search-tree/binary_search_tree.ml
@@ -1,16 +1,22 @@
 open Base
 
-type bst
-
-let empty = failwith "'empty' is missing"
-
-let value _ = failwith "'value' is missing"
-
-let left _ = failwith "'left' is missing"
-
-let right _ = failwith "'right' is missing"
-
-let insert _ _ = failwith "'insert' is missing"
-
-let to_list _ = failwith "'to_list' is missing"
-
+type bst =
+  | Leaf
+  | Node of bst * int * bst
+let empty = Leaf
+let value = function
+  | Leaf            -> Error "Tree is empty"
+  | (Node(_, v, _)) -> Ok v
+let left = function
+  | Leaf            -> Error "Tree is empty"
+  | (Node(l, _, _)) -> Ok l
+let right = function
+  | Leaf            -> Error "Tree is empty"
+  | (Node(_, _, r)) -> Ok r
+let rec insert n = function
+  | Leaf                     -> Node(Leaf, n, Leaf)
+  | Node(l, v, r) when n > v -> Node(l, v, insert n r)
+  | Node(l, v, r)            -> Node(insert n l, v, r)
+let rec to_list = function
+  | Leaf          -> []
+  | Node(l, v, r) -> List.concat [to_list l; [v]; to_list r]

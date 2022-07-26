dcb1eed7064d4c6952290ba313b12bc0924d7de4
2Paradise <robinnw@naver.com>
2022-07-26 13:53:33 +0900

    fix: solution difference of squares

--- difference-of-squares/difference_of_squares.ml
+++ difference-of-squares/difference_of_squares.ml
@@ -1,8 +1,13 @@
-let square_of_sum _ = 
-    failwith "'square_of_sum' is missing"
+let square n = n * n
 
-let sum_of_squares _ =
-    failwith "'sum_of_squares' is missing"
+let square_of_sum n = 
+    let rec cal_s_of_sq acc n = if n < 1 then acc else cal_s_of_sq (n + acc) (n - 1) in
+    square (cal_s_of_sq 0 n)
 
-let difference_of_squares _ =
-    failwith "'difference_of_squares' is missing"
+let sum_of_squares n =
+    let rec cal_sq_of_s acc n = 
+        if n < 1 then acc else cal_sq_of_s (square n + acc) (n - 1) in
+    cal_sq_of_s 0 n
+
+let difference_of_squares n =
+    (square_of_sum n) - (sum_of_squares n)
